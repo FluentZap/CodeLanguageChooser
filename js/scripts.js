2a0c63f46489428e167a8a8596b86d63ad066bdc
Nathan <FluentZap@Gmail.com>
2019-03-29 13:55:47 -0700

    compare options based on weight reset when button is pressed

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+var chosen;
+
 $(document).ready(function () {
   $("#form-language-test").submit(function (event) {
     event.preventDefault();
@@ -14,24 +16,87 @@ $(document).ready(function () {
 
     if (focus === undefined) shakeMeRed("#form-titleFocus"), notdone = true;
     if (platform === undefined) shakeMeRed("#form-titlePlatform"), notdone = true;
-    if (design === undefined) shakeMeRed("#form-titleDesign"), notdone = true;
     if (goals === undefined) shakeMeRed("#form-titleGoals"), notdone = true;
     if (style === undefined) shakeMeRed("#form-titleStyle"), notdone = true;
     if (hobby === undefined) shakeMeRed("#form-titleHobby"), notdone = true;
+    if (notdone) return;
 
-    if (notdone)
-      return;
+    var cSharp = 0, php = 0, ruby = 0, java = 0;
+    var color;
+    // Scale of 0, 1 or 2 as weight for the quesitons
+    if (focus === "option1") cSharp += 1, php += 0, ruby += 2, java += 1;
+    if (focus === "option2") cSharp += 2, php += 2, ruby += 0, java += 1;
 
+    if (platform === "option1") cSharp += 1, php += 2, ruby += 2, java += 1;
+    if (platform === "option2") cSharp += 2, php += 0, ruby += 0, java += 1;
+    if (platform === "option3") cSharp += 1, php += 0, ruby += 0, java += 2;
+    if (design === "on") cSharp += 0, php += 0, ruby += 1, java += 0;
 
+    if (goals === "option1") cSharp += 1, php += 1, ruby += 2, java += 0;
+    if (goals === "option2") cSharp += 1, php += 0, ruby += 0, java += 1;
+    if (goals === "option3") cSharp += 2, php += 1, ruby += 1, java += 2;
 
+    if (style === "option1") cSharp += 1, php += 1, ruby += 0, java += 1;
+    if (style === "option2") cSharp += 1, php += 0, ruby += 0, java += 1;
+    if (style === "option3") cSharp += 1, php += 1, ruby += 0, java += 1;
+
+    if (hobby === "option1") cSharp += 2, php += 1, ruby += 0, java += 2;
+    if (hobby === "option2") cSharp += 1, php += 2, ruby += 2, java += 0;
+    if (hobby === "option3") cSharp += 1, php += 2, ruby += 2, java += 0;
+
+    if (cSharp >= php && cSharp >= ruby && cSharp >= java) chosen = "#cSharp";
+    if (php >= cSharp && php >= ruby && php >= java) chosen = "#php";
+    if (ruby >= php && ruby >= cSharp && ruby >= java) chosen = "#ruby";
+    if (java >= php && java >= ruby && java >= cSharp) chosen = "#java";
+
+    if (chosen === "#cSharp")
+      color = "#c693c6"
+    else if (chosen === "#php")
+      color = "#a0bcef"
+    else if (chosen === "#ruby")
+      color = "#f27b7b"
+    else if (chosen === "#java")
+      color = "#93785e"
 
     $("#form-language-test").toggle("fade", { direction: 'up', mode: 'hide'}, 1000, function () {
-      $("body").animate({backgroundColor: "#f27b7b"}, 'slow');
-      $("#ruby").toggle("slide", { direction: 'down', mode: 'show'}, 1000);
+      $("body").animate({backgroundColor: color}, 'slow');
+      $(chosen).toggle("slide", { direction: 'down', mode: 'show'}, 1000);
     });
+
   });
+
+  $(".retake").click(function () {
+    //$(element).is(":visible");
+
+    $(chosen).hide("fade", { direction: 'down', mode: 'hide'}, 1000, function () {
+      $("body").animate({backgroundColor: "#bebbbb"}, 'slow');
+      resetForm();
+      $("#form-language-test").show("slide", { direction: 'up', mode: 'show'}, 1000);
+    });
+
+
+  });
+
+
+
 });
 
+function resetForm() {
+  $('input[name=form-focus]').prop('checked', false);
+  $('input[name=form-platform]').prop('checked', false);
+
+  $('#form-design').prop('checked', false);
+
+  $('input[name=form-goals]').prop('checked', false);
+  $('input[name=form-style]').prop('checked', false);
+  $('input[name=form-hobby]').prop('checked', false);
+
+  $('#form-titleFocus').removeClass("notAnswered");
+  $('#form-titlePlatform').removeClass("notAnswered");
+  $('#form-titleGoals').removeClass("notAnswered");
+  $('#form-titleStyle').removeClass("notAnswered");
+  $('#form-titleHobby').removeClass("notAnswered");
+}
 
 
 
