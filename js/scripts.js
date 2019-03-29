82edeb1c83074453ec9a234a452044a6a7c3676c
Nathan <FluentZap@Gmail.com>
2019-03-29 10:22:43 -0700

    fade to color based on selection

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
     //$("#ruby").fadeIn();
 
     $("#form-language-test").toggle("fade", { direction: 'up', mode: 'hide'}, 1000, function () {
+      $("body").animate({backgroundColor: "#f27b7b"}, 'slow');
       $("#ruby").toggle("slide", { direction: 'down', mode: 'show'}, 1000);
     });
 
